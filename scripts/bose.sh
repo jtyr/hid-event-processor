6794aa286e6d9b1b1458cc5416c3f4e1f139bc2f
Jiri Tyr <jiri.tyr@gmail.com>
2022-01-28 10:18:33 +0000

    Improvements

--- scripts/bose.sh
+++ scripts/bose.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
+# Input parameters
 ACTION=${1:-toggle}
 DEVICE=${2:-/dev/input/event23}
 
-evtest --query "$DEVICE" EV_LED LED_MUTE
+# Name of the device to be muted
+CARD='alsa_input.usb-Bose_Bose_USB_Link_082063Z03330148AE-00.mono-fallback'
 
-RC="$?"
+# Check if the device is muted or not
+MUTED=$(pactl get-source-mute "$CARD" | sed 's/^Mute: //')
 
+# Decide what to do
 if [[ $ACTION == 'toggle' ]]; then
-    if [[ $RC == 0 ]]; then
-        evemu-event "$DEVICE" --type 17 --code 7 --value 1
-        pactl set-source-mute alsa_input.usb-Bose_Bose_USB_Link_082063Z03330148AE-00.mono-fallback 1
+    # Change USB LED accordingly
+    if [[ $MUTED == 'yes' ]]; then
+        evemu-event "$DEVICE" --type 17 --code 7 --value 0
     else
-        evemu-event "$DEVICE" --type 17 --code 7 --value 0
-        pactl set-source-mute alsa_input.usb-Bose_Bose_USB_Link_082063Z03330148AE-00.mono-fallback 0
+        evemu-event "$DEVICE" --type 17 --code 7 --value 1
     fi
+
+    # Toggle the device mute
+    pactl set-source-mute "$CARD" toggle
 elif [[ $ACTION == 'hangup' ]]; then
-    if [[ $RC != 0 ]]; then
+    # Unmute at the end of the call
+    if [[ $MUTED == 'yes' ]]; then
         evemu-event "$DEVICE" --type 17 --code 7 --value 0
+        pactl set-source-mute "$CARD" 0
     fi
 else
     echo "Unknown action: $ACTION"
